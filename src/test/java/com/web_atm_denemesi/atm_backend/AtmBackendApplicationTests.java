9f56d2edb831b9f6c07a9136ada187d852ca7689
Yiğit Çoban <yigitcoban916@gmail.com>
2026-08-06 16:05:04 +0300

    feat: inital spring boot backend setup

---
+++ src/test/java/com/web_atm_denemesi/atm_backend/AtmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_atm_denemesi.atm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
