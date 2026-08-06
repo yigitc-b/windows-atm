c1879d1103c7df7f7f8f9261e86763fdc2353561
Yiğit Çoban <yigitcoban916@gmail.com>
2026-08-06 23:27:13 +0300

    1.0.0

--- src/main/java/com/web_atm_denemesi/atm_backend/controller/WebController.java
+++ src/main/java/com/web_atm_denemesi/atm_backend/controller/WebController.java
@@ -4,35 +4,47 @@ import com.web_atm_denemesi.atm_backend.dto.LoginRequest;
 import com.web_atm_denemesi.atm_backend.dto.ServiceResponse;
 import com.web_atm_denemesi.atm_backend.service.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
-public class LoginController {
+public class WebController {
 
     private final AuthService authService;
+    @Value("${info.app.version}")
+    private String version;
 
     // Dependency Injection (Spring'in AuthService'i otomatik bağlaması)
     @Autowired
-    public LoginController(AuthService authService) {
+    public WebController(AuthService authService) {
         this.authService = authService;
     }
 
     @GetMapping("/login")
     public String getLoginPage() {
-        return "forward:/login.html";
+        return "login.html";
     }
 
     @GetMapping("/about")
-    public String about() {
+    public String getAboutPage() {
         return "about.html";
     }
 
     @GetMapping("/dashboard")
-    public String dashboard() {
+    public String getDashboardPage() {
         return "dashboard.html";
     }
+
+    @GetMapping("/api/version")
+    @ResponseBody
+    public String getVersion() {
+        return "Application Version: " + version;
+    }
+    
 
     @PostMapping("/api/login")
     @ResponseBody
