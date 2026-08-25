5140cb28cd8894a705e425ef0342793f4d7f3311
Yiğit Çoban <yigitcoban916@gmail.com>
2026-08-25 17:34:59 +0300

    1.0.1 ANA SÜRÜM

---
+++ src/main/java/com/web_atm_denemesi/atm_backend/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.web_atm_denemesi.atm_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+    
+}
